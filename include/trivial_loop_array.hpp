00acbb7dd4eafba2225babb7b3680a2077a57f66
Dmitry Isupov <TheApX@users.noreply.github.com>
2021-08-17 23:13:40 +0000

    trivial_loop_array implementation

--- include/trivial_loop_array.hpp
+++ include/trivial_loop_array.hpp
@@ -1,1 +1,149 @@
 #pragma once
+
+#include <cstddef>
+#include <stdexcept>
+#include <type_traits>
+
+namespace theapx {
+
+template <class T, size_t kCapacity, class Enable = void>
+class trivial_loop_array {};  // primary template
+
+template <class T, size_t kCapacity>
+class trivial_loop_array<
+    T, kCapacity, typename std::enable_if<std::is_trivial<T>::value>::type> {
+ public:
+  template <class Owner, class P>
+  class iterator_base {
+   public:
+    P& operator*() { return owner_->at(pos_); }
+    P* operator->() { return &owner_->at(pos_); }
+
+    iterator_base<Owner, P> operator--() {
+      --pos_;
+      return *this;
+    }
+    iterator_base<Owner, P> operator--(int) {
+      iterator_base<Owner, P> result = *this;
+      --pos_;
+      return result;
+    }
+    iterator_base<Owner, P> operator++() {
+      ++pos_;
+      return *this;
+    }
+    iterator_base<Owner, P> operator++(int) {
+      iterator_base<Owner, P> result = *this;
+      ++pos_;
+      return result;
+    }
+
+    bool operator==(const iterator_base<Owner, P>& other) const {
+      return this->owner_ == other.owner_ && this->pos_ == other.pos_;
+    }
+    bool operator!=(const iterator_base<Owner, P>& other) const {
+      return !operator==(other);
+    }
+
+    iterator_base(const iterator_base<Owner, P>& other)
+        : owner_(other.owner_), pos_(other.pos_) {}
+    iterator_base(Owner* owner, size_t pos) : owner_(owner), pos_(pos) {}
+
+   private:
+    Owner* owner_;
+    int pos_;
+  };
+
+  using iterator = iterator_base<trivial_loop_array, T>;
+  using const_iterator = iterator_base<const trivial_loop_array, const T>;
+
+  iterator begin() { return iterator(this, 0); }
+  iterator end() { return iterator(this, array_size); }
+  const_iterator begin() const { return const_iterator(this, 0); }
+  const_iterator end() const { return const_iterator(this, array_size); }
+
+  T& at(size_t pos);
+  const T& at(size_t pos) const;
+  T& operator[](size_t pos) { return at(pos); }
+  const T& operator[](size_t pos) const { return at(pos); }
+
+  size_t capacity() const { return kCapacity; }
+  size_t size() const { return array_size; }
+  bool empty() const { return size() == 0; }
+
+  void reserve(size_t){/*no-op*/};
+  void resize(size_t new_size);
+
+  void push_back(const T& v);
+  void push_front(const T& v);
+
+  // ---------- data ----------
+  T data[kCapacity];
+  size_t start_index;
+  size_t array_size;
+};
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+template <class T, size_t kCapacity>
+inline T& trivial_loop_array<
+    T, kCapacity,
+    typename std::enable_if<std::is_trivial<T>::value>::type>::at(size_t pos) {
+  if (pos >= array_size || pos < 0) {
+    throw std::out_of_range("pos");
+  }
+  return data[(start_index + pos) % kCapacity];
+}
+
+template <class T, size_t kCapacity>
+inline const T& trivial_loop_array<
+    T, kCapacity,
+    typename std::enable_if<std::is_trivial<T>::value>::type>::at(size_t pos)
+    const {
+  if (pos >= array_size || pos < 0) {
+    throw std::out_of_range("pos");
+  }
+  return data[(start_index + pos) % kCapacity];
+}
+
+template <class T, size_t kCapacity>
+inline void trivial_loop_array<
+    T, kCapacity, typename std::enable_if<std::is_trivial<T>::value>::type>::
+    resize(size_t new_size) {
+  if (new_size > kCapacity || new_size < 0) {
+    throw std::out_of_range("new_size");
+  }
+  array_size = new_size;
+}
+
+template <class T, size_t kCapacity>
+inline void trivial_loop_array<
+    T, kCapacity, typename std::enable_if<std::is_trivial<T>::value>::type>::
+    push_back(const T& v) {
+  if (array_size == kCapacity) {
+    throw std::length_error("trivial_loop_array capacity exceeded");
+  }
+
+  ++array_size;
+  at(array_size - 1) = v;
+}
+
+template <class T, size_t kCapacity>
+inline void trivial_loop_array<
+    T, kCapacity, typename std::enable_if<std::is_trivial<T>::value>::type>::
+    push_front(const T& v) {
+  if (array_size == kCapacity) {
+    throw std::length_error("trivial_loop_array capacity exceeded");
+  }
+
+  ++array_size;
+  if (start_index == 0) {
+    start_index = kCapacity - 1;
+  } else {
+    --start_index;
+  }
+
+  at(0) = v;
+}
+
+}  // namespace theapx
--- test/trivial_loop_array_test.cpp
+++ test/trivial_loop_array_test.cpp
@@ -2,3 +2,175 @@
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+
+namespace {
+
+static_assert(std::is_trivial<theapx::trivial_loop_array<int, 10>>::value);
+
+using ::testing::Eq;
+using ::testing::IsTrue;
+
+TEST(TrivialLoopArray, EmptyInitializer) {
+  theapx::trivial_loop_array<int, 10> array{};
+
+  EXPECT_THAT(array.size(), Eq(0));
+  EXPECT_THAT(array.empty(), IsTrue());
+}
+
+TEST(TrivialLoopArray, DesignatedInitializer) {
+  theapx::trivial_loop_array<int, 10> array{
+      .data = {0, 0, 1, 2, 3},
+      .start_index = 2,
+      .array_size = 3,
+  };
+
+  EXPECT_THAT(array.size(), Eq(3));
+  EXPECT_THAT(array[0], Eq(1));
+  EXPECT_THAT(array[1], Eq(2));
+  EXPECT_THAT(array[2], Eq(3));
+}
+
+TEST(TrivialLoopArray, WrapsAround) {
+  theapx::trivial_loop_array<int, 6> array{
+      .data = {4, 0, 0, 1, 2, 3},
+      .start_index = 3,
+      .array_size = 4,
+  };
+
+  EXPECT_THAT(array.size(), Eq(4));
+  EXPECT_THAT(array[0], Eq(1));
+  EXPECT_THAT(array[1], Eq(2));
+  EXPECT_THAT(array[2], Eq(3));
+  EXPECT_THAT(array[3], Eq(4));
+}
+
+TEST(TrivialLoopArray, ResizeUp) {
+  theapx::trivial_loop_array<int, 10> array{
+      .data = {0, 0, 1, 2, 3},
+      .start_index = 2,
+      .array_size = 3,
+  };
+
+  array.resize(5);
+
+  EXPECT_THAT(array.size(), Eq(5));
+  EXPECT_THAT(array[0], Eq(1));
+  EXPECT_THAT(array[1], Eq(2));
+  EXPECT_THAT(array[2], Eq(3));
+  EXPECT_THAT(array[3], Eq(0));
+  EXPECT_THAT(array[4], Eq(0));
+}
+
+TEST(TrivialLoopArray, ResizeDown) {
+  theapx::trivial_loop_array<int, 10> array{
+      .data = {0, 0, 1, 2, 3},
+      .start_index = 2,
+      .array_size = 3,
+  };
+
+  array.resize(2);
+
+  EXPECT_THAT(array.size(), Eq(2));
+  EXPECT_THAT(array[0], Eq(1));
+  EXPECT_THAT(array[1], Eq(2));
+}
+
+TEST(TrivialLoopArray, RangeLoop) {
+  theapx::trivial_loop_array<int, 10> array{
+      .data = {0, 0, 1, 2, 3},
+      .start_index = 2,
+      .array_size = 3,
+  };
+
+  int n = 1;
+  for (int& v : array) {
+    EXPECT_THAT(v, Eq(n));
+    ++n;
+  }
+}
+
+TEST(TrivialLoopArray, RangeLoopConst) {
+  theapx::trivial_loop_array<int, 10> mutable_array{
+      .data = {0, 0, 1, 2, 3},
+      .start_index = 2,
+      .array_size = 3,
+  };
+
+  const theapx::trivial_loop_array<int, 10>& array = mutable_array;
+
+  int n = 1;
+  for (const int& v : array) {
+    EXPECT_THAT(v, Eq(n));
+    ++n;
+  }
+}
+
+TEST(TrivialLoopArray, PushBack) {
+  theapx::trivial_loop_array<int, 10> array{
+      .data = {0, 0, 1, 2, 3},
+      .start_index = 2,
+      .array_size = 3,
+  };
+
+  array.push_back(100);
+
+  EXPECT_THAT(array.size(), Eq(4));
+  EXPECT_THAT(array[0], Eq(1));
+  EXPECT_THAT(array[1], Eq(2));
+  EXPECT_THAT(array[2], Eq(3));
+  EXPECT_THAT(array[3], Eq(100));
+}
+
+TEST(TrivialLoopArray, PushBackWrapsAround) {
+  theapx::trivial_loop_array<int, 5> array{
+      .data = {0, 0, 1, 2, 3},
+      .start_index = 2,
+      .array_size = 3,
+  };
+
+  array.push_back(100);
+
+  EXPECT_THAT(array.size(), Eq(4));
+  EXPECT_THAT(array[0], Eq(1));
+  EXPECT_THAT(array[1], Eq(2));
+  EXPECT_THAT(array[2], Eq(3));
+  EXPECT_THAT(array[3], Eq(100));
+}
+
+TEST(TrivialLoopArray, PushFront) {
+  theapx::trivial_loop_array<int, 10> array{
+      .data = {0, 0, 1, 2, 3},
+      .start_index = 2,
+      .array_size = 3,
+  };
+
+  array.push_front(100);
+
+  EXPECT_THAT(array.size(), Eq(4));
+  EXPECT_THAT(array[0], Eq(100));
+  EXPECT_THAT(array[1], Eq(1));
+  EXPECT_THAT(array[2], Eq(2));
+  EXPECT_THAT(array[3], Eq(3));
+
+  EXPECT_THAT(array.start_index, Eq(1));
+}
+
+TEST(TrivialLoopArray, PushFrontWrapsAround) {
+  theapx::trivial_loop_array<int, 10> array{
+      .data = {1, 2, 3},
+      .start_index = 0,
+      .array_size = 3,
+  };
+
+  array.push_front(100);
+
+  EXPECT_THAT(array.size(), Eq(4));
+  EXPECT_THAT(array[0], Eq(100));
+  EXPECT_THAT(array[1], Eq(1));
+  EXPECT_THAT(array[2], Eq(2));
+  EXPECT_THAT(array[3], Eq(3));
+
+  EXPECT_THAT(array.start_index, Eq(array.capacity() - 1));
+}
+
+}  // namespace
